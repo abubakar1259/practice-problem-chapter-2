60b51adb7dc16db209b9f3dd6dc3b365e2a8b1c2
abubakar1259 <57959814+abubakar1259@users.noreply.github.com>
2021-08-05 07:46:05 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+import random
+
+def game(comp,you):
+    if comp == you:
+        return None
+    elif comp == "s":
+        if you == "w":
+            return False
+        elif you == "g":
+            return True
+    elif comp == "w":
+        if you == "g":
+            return False
+        elif you == "s":
+            return True
+    elif comp == "g":
+        if you == "s":
+            return False
+        elif you == "w":
+            return True
+
+
+
+
+print("comp turn: Snake(s) Water(w) or Gun(g)?\n")
+randno = random.randint(1,3)
+if randno == 1:
+    comp = "s"
+elif randno == 2:
+    comp = "w"
+elif randno == 3:
+    comp = "g"
+you = input("your turn: Snake(s) Water(w) or Gun(g)?\n")
+a = game(comp,you)    # game = module work for game    
+
+print(f"computer chose {comp}")
+print(f"you choose {you}")
+
+if a == None:
+    print("The game are tie!")
+elif a:
+    print("YOU WiN!")
+else:
+    print("YOU LOSE!") 
